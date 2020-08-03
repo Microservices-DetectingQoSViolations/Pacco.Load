1bb2657123ebcbf97d82d08e5ac572184835a9b2
ficmichal <meho.fic@gmail.com>
2020-08-04 00:07:15 +0200

    Apply basic user to "vehicles" user.

--- load/scenarios/fun_with_vehicles.py
+++ load/scenarios/fun_with_vehicles.py
@@ -1,11 +1,12 @@
-from locust import TaskSet, task
+from locust import task
 from fake import identity, customer, vehicles
 from json import dumps
 from settings import httpSettings, add_auth
-from http_utils.on_response_actions import get_access_token, get_resource_id
+from http_utils.on_response_actions import get_resource_id
+from load.scenarios.basic_user import BasicUserSection
 
 
-class FunWithParcels(TaskSet):
+class FunWithVehicles(BasicUserSection):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.access_token = ''
@@ -17,13 +18,9 @@ class FunWithParcels(TaskSet):
         self.vehicle_ids = []
 
     def on_start(self):
-        self.client.post("/identity/sign-up", dumps(self.user_data), headers=httpSettings['content_header'])
-        with self.client.post("/identity/sign-in", dumps(self.login_data),
-                              headers=httpSettings['content_header']) as response:
-            self.access_token = get_access_token(response)
-            self.client.get("/identity/me", headers=add_auth({}, self.access_token))
-            self.client.post("/customers", dumps(self.customer_data),
-                             headers=add_auth(httpSettings['content_header'], self.access_token))
+        super().on_start()
+        self.add_vehicle()
+        self.add_vehicle()
 
     @task(5)
     def add_vehicle(self):
@@ -40,13 +37,3 @@ class FunWithParcels(TaskSet):
         cap, var = vehicles.browse_vehicle()
         self.client.get(f"/vehicles?payloadCapacity={cap}&loadingCapacity={cap}&variants={var}",
                         headers=add_auth({}, self.access_token))
-
-    @task(1)
-    def delete_vehicle(self):
-        self.client.delete(f"/parcels/{vehicles.any_vehicle(self.vehicle_ids)}", headers=add_auth({}, self.access_token))
-
-    @task(1)
-    def sing_in(self):
-        with self.client.post("/identity/sign-in", dumps(self.login_data),
-                              headers=httpSettings['content_header']) as response:
-            self.access_token = get_access_token(response)
