c9ccf79fe7972c4dbe1bc4b6552529e21a349ec9
ficmichal <meho.fic@gmail.com>
2020-08-08 14:08:23 +0200

    Fix faking deliveries data.

--- fake/deliveries.py
+++ fake/deliveries.py
@@ -8,7 +8,7 @@ Faker.seed(fake_settings['seed'])
 def start_delivery(order_id, date):
     return {
         "orderId": order_id,
-        "description": faker.words(faker.random.randint(0, 15)),
+        "description": fake_description(),
         "dateTime": date
     }
 
@@ -16,19 +16,23 @@ def start_delivery(order_id, date):
 def fail(delivery_id):
     return {
         "id": delivery_id,
-        "reason": faker.words(faker.random.randint(0, 15))
+        "reason": fake_description()
     }
 
 
 def registrations(delivery_id, date):
     return {
         "id": delivery_id,
-        "description": faker.words(faker.random.randint(0, 15)),
+        "description": fake_description(),
         "dateTime": date
     }
 
 
-def complete(date):
+def complete(delivery_id):
     return {
-        "id": date
+        "id": delivery_id
     }
+
+
+def fake_description():
+    return ' '.join(faker.words(faker.random.randint(0, 15)))
