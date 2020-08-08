92a454792da2fcc50c6901532812bd8c4f511d45
ficmichal <meho.fic@gmail.com>
2020-08-08 14:09:04 +0200

    Fix faking orders data.

--- fake/orders.py
+++ fake/orders.py
@@ -6,7 +6,7 @@ Faker.seed(fake_settings['seed'])
 
 
 def fake_order_date():
-    return faker.date_this_year(before_today=False, after_today=True)
+    return str(faker.date_this_year(before_today=False, after_today=True))
 
 
 def assign_date(date):
