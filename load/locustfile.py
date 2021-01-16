2bb68915992ee8e2c85cd09e8f8e524a3db43a78
ficmichal <meho.fic@gmail.com>
2021-01-16 13:27:55 +0100

    Change load shape

--- load/locustfile.py
+++ load/locustfile.py
@@ -3,23 +3,81 @@ from load.scenarios.basic_user import BasicUserSection
 from load.scenarios.parcel_spammer import ParcelSpammer
 from load.scenarios.vehicle_spammer import VehicleSpammer
 from load.scenarios.full_scenario import FullScenario
+from locust import LoadTestShape
+from locust.env import Environment
+from locust.log import setup_logging
+
+import numpy as np
+import random
+import time
 
 
-class BasicSpammerUser(HttpUser):
-    wait_time = between(0.8, 1.2)
-    tasks = [BasicUserSection]
+setup_logging("INFO", None)
+iters = 42
+run_times = []
+
+for iter in range(iters):
+    class BasicSpammerUser(HttpUser):
+        wait_time = between(1.2, 1.6)
+        tasks = [BasicUserSection]
 
 
-class ParcelSpammerUser(HttpUser):
-    wait_time = between(0.8, 1.2)
-    tasks = [ParcelSpammer]
+    class ParcelSpammerUser(HttpUser):
+        wait_time = between(1.2, 1.6)
+        tasks = [ParcelSpammer]
 
 
-class VehicleSpammerUser(HttpUser):
-    wait_time = between(0.8, 1.2)
-    tasks = [VehicleSpammer]
+    class VehicleSpammerUser(HttpUser):
+        wait_time = between(1.2, 1.6)
+        tasks = [VehicleSpammer]
 
 
-class FullScenarioUser(HttpUser):
-    wait_time = between(1., 1.5)
-    tasks = [FullScenario]
+    class FullScenarioUser(HttpUser):
+        wait_time = between(1.5, 2.3)
+        tasks = [FullScenario]
+
+
+    env = Environment(user_classes=[ParcelSpammerUser, VehicleSpammerUser, FullScenarioUser],
+                      host='http://192.168.0.220:32120', reset_stats=True)
+    env.create_local_runner()
+
+    start = time.time()
+
+
+    class StagesShape(LoadTestShape):
+        stages_len = random.randint(8, 18)
+
+        step = int(420 / stages_len)
+
+        stages_time = np.linspace(step, 420, stages_len)
+        stages = []
+        users = 15
+        spawn_rate = 0
+        for st in stages_time:
+            users = users + random.randint(6, 15)
+            spawn_rate = random.randint(1, 3)
+            stages.append({'duration': int(st), 'users': users, 'spawn_rate': spawn_rate})
+
+        def tick(self):
+            run_time = self.get_run_time()
+
+            for stage in self.stages:
+                if run_time < stage["duration"]:
+                    tick_data = (stage["users"], stage["spawn_rate"])
+                    return tick_data
+            return None
+    stages_shape = StagesShape()
+    stages_shape.reset_time()
+    env.runner.start_shape(stages_shape)
+
+    # in 420 seconds stop the runner
+    time.sleep(420)
+
+    env.runner.quit()
+
+    stop = time.time()
+    with open('../testdata/spam9.txt', 'a') as spam_data:
+        spam_data.write(f'{stop}, ')
+
+    time.sleep(180)
+    env = None
